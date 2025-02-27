8aa8ddbdd5b4e1a40e883034fe0591d7c2d3918e
Drew Wolfe <wolfeaw@mail.uc.edu>
2025-02-26 21:54:01 -0500

    Finished invoking the methods from jar_jar to main.py and added documentation for main.py

--- TeamGlóin_Assignment06/main_package/main.py
+++ TeamGlóin_Assignment06/main_package/main.py
@@ -1,5 +1,25 @@
 # main.py
-# TeamGlóin_Assignment06
+# Name: Drew Wolfe
+# Email: wolfeaw@mail.uc.edu
+# Assignment Number: Assignment 06
+# Due Date: 2/27/25
+# Course #/Section: IS4010-002
+# Semester/Year: Spring 2025
+# Brief description of the assignment: Use OOP with a partner
+# Brief description of what this module does: Instantiates the JarJarBinks class
+# Citations: 
+# Anything else that's relevant:
 
+from jarjarbinks_package.jar_jar import JarJarBinks
 if __name__ == "__main__":
-    pass
+    jarjar = JarJarBinks()
+
+    print("Initial State:", jarjar)  
+    print("Debug Representation:", repr(jarjar))  
+    print("Mishap 1:", jarjar.cause_mishap())
+    print("Mishap 2:", jarjar.cause_mishap())
+    print("Energy Before:", jarjar.energy_level) 
+    
+    jarjar.energy_level = 50
+    print("Energy After Manual Change:", jarjar.energy_level)
+    print("Final State:", jarjar)
